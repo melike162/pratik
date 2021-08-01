cbb5f82a46917f4fc2fa3308a78981ee791e3cbe
melike işeri <86420860+melike162@users.noreply.github.com>
2021-08-01 10:55:08 +0300

    Add files via upload

--- Pj1.java
+++ Pj1.java
@@ -1,22 +1,22 @@
 package Paket4;
 import java.util.Scanner;
 public class Pj1 {
-    static boolean isPolidrom(int number){
+    static void isPolidrom(int number){
         int temp=number,reversNumber=0,lastNumber;
         while(temp!=0){
             lastNumber=temp%10;
             reversNumber=reversNumber*10+lastNumber;
             temp/=10;
         }
-        if (number==reversNumber){ return true;}
-        else { return false; }
+        if (number==reversNumber){ System.out.println("Palindrom sayıdır.");}
+        else { System.out.println("Palindrom sayı değildir"); }
     }
     public static void main(String[] args) {
         int sayi;
         Scanner inpu = new Scanner(System.in);
         System.out.print("bir sayı giriniz: ");
         sayi = inpu.nextInt();
-        System.out.println(isPolidrom(sayi));
+        isPolidrom(sayi);
 
     }
 }
