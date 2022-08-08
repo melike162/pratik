07edb0479ef9a01c67a4436c9e301a472f34e650
melike işeri <86420860+melike162@users.noreply.github.com>
2022-08-08 19:07:52 +0300

    Add files via upload

---
+++ Main_PatikaStore.java
@@ -0,0 +1,8 @@
+package Paket7_Collection_CokSıkKullanılıyor_ONEMLI.Odev3;
+
+public class Main_PatikaStore {
+    public static void main(String[] args) {
+        Adding adding=new Adding();
+        adding.adding();
+    }
+}
